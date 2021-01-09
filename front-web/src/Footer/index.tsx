91f83d363593449e2e923dee37bf7cd9c6994fa8
unknown <lucas.fernandes97@hotmail.com>
2021-01-08 23:47:01 -0300

    Modified footer index

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import { ReactComponent as YoutubeIcon } from './youtube.svg';
-import { ReactComponent as InstagramIcon } from './linkedin.svg';
-import { ReactComponent as LinkedinIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
 
 function Footer() {
     return (
